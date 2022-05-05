c77d68f4886113f2d4510730509a4c2a408e58d3
CAIO <caioeduardosantos7@gmail.com>
2022-05-04 23:02:21 -0300

    menu repots

---
+++ src/components/WidgetForm/index.tsx
@@ -0,0 +1,55 @@
+import { CloseButton } from "../CloseButton";
+import bugImageUrl from '../../assets/bug.svg';
+import ideaImageUrl from '../../assets/ideia.svg'
+import outroImageUrl from '../../assets/outro.svg'
+import { useState } from "react";
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+
+
+export const feedbackTypes = {
+    BUG: {
+        title: 'Problema',
+        image: {
+            source: bugImageUrl,
+            alt:'Image de um inseto'
+        },
+    },
+    IDEA: {
+        title: 'Ideia',
+        image: {
+            source: ideaImageUrl,
+            alt: 'Imagem de uma lâmpada'
+        },
+    },
+    OTHER: {
+        title: 'Outros',
+        image: { 
+            source: outroImageUrl,
+            alt: 'Imagem de um balão de pensamento'
+        },
+    }, 
+};
+
+export type FeedbackType = keyof typeof feedbackTypes;
+
+export function WidgetForm(){
+    const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+    return (
+        <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+            <header>
+                <span className="text-xl leading-6">Deixe seu feedback</span>
+               <CloseButton />         
+            </header>
+            {!feedbackType ?(
+                <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+            ): (
+                <p>Hello word</p>
+            )}
+           
+            <footer className="text-neutral-400">
+                Feito pelas equipes tabajara <a className="underline underline-offset-2" href="https://www.linkedin.com/in/caio-eduardo-ba78601b3/">CaioEd77</a> 
+               
+            </footer>
+        </div>
+    );
+}
